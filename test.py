227f7f37eb6bb0602320d379da9a2a8389a62762
Titanop242424 <optitan317@gmail.com>
2025-06-20 11:10:40 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello!")
+
+def main():
+    token = "7788865701:AAH0RXiPO73BtQuRWzieAdhs2nQerscAvk0"
+    app = ApplicationBuilder().token(token).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
